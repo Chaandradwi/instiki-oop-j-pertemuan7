4afc3a23250de29c93180e3500d9955f3bc4fc98
Chaandradwi <dwichaandra2004@gmail.com>
2025-04-29 21:16:08 +0800

    Keterangan commit

---
+++ pertemuan7/src/pertemuan7/Pertemuan7.java
@@ -0,0 +1,20 @@
+package pertemuan7;
+
+/**
+ *
+ * @author Chaandradwi
+ */
+public class Pertemuan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        mahasiswa mhs = new mahasiswa();
+        mhs.datamhs();
+        mhs.datamhs("1234567");
+        mhs.datamhs("2301010458", "chan");
+        
+    }
+    
+}
